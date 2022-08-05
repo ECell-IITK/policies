29c8487df59aa1f68b3cdad609091c35dcf244c4
Arpit Kaithwar <arpitkaithwar02@gmail.com>
2022-08-05 23:17:29 +0530

    Animation Updated

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,6 +4,18 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 })
 
+// Scroll Button 
+const Scrollbtn = document.querySelector(".scroll")
+
+window.addEventListener("scroll", function(){
+    Scrollbtn.classList.toggle("active", window.scrollY > 500);
+})
+
+Scrollbtn.addEventListener("click", () =>{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
 // Navigation effect on Scrolling
 
 window.addEventListener("scroll", () => {
@@ -68,3 +80,23 @@ navItem.forEach((navItem) =>{
         navigation.classList.remove("active");
     })
 })
+
+
+// Scroll Reveal Animation
+ScrollReveal({
+    reset: true,
+    distance: '46px',
+    duration: 1750,
+    delay: 85
+});
+
+ScrollReveal().reveal('.home .home-container h1, .section-title-01, .section-title-02', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.home .home-container h3, .home .home-container p', { delay: 600, origin: 'right' });
+ScrollReveal().reveal('.home .home-container .btn', { delay: 700, origin: 'bottom' });
+ScrollReveal().reveal('.contact-left li,', { delay: 500, origin: 'left', interval: 200 });
+ScrollReveal().reveal('.policy, .policy-mid, .center-line, .timeline-content, .footer-container .info', { delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.content .info img, .contact-right, .para1, .img1 img', { delay: 600, origin: 'right' });
+ScrollReveal().reveal('.content .info p, .contact-left h2', { delay: 700, origin: 'left'});
+ScrollReveal().reveal('.content .info p, .contact-left h2, .para', { delay: 600, origin: 'left'});
+ScrollReveal().reveal('.block, .profile-card', { delay: 700, origin: 'bottom', interval:200 });
+ScrollReveal().reveal('footer .group', { delay: 500, origin: 'top', interval:200 });
